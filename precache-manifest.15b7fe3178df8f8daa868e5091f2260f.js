6794e93fde2d6e3b589051dcb7734454a304f9e5
vasiliy <gladishevvasiliy@gmail.com>
2018-11-09 01:00:44 +0300

    Updates

---
+++ precache-manifest.15b7fe3178df8f8daa868e5091f2260f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "68389de4bf478df26290bef1eee3ce71",
+    "url": "/kruki/static/media/Kruk-Kalashn-edit-VG.68389de4.ttf"
+  },
+  {
+    "revision": "3dc112bbf71702bc317e",
+    "url": "/kruki/static/js/runtime~main.3dc112bb.js"
+  },
+  {
+    "revision": "a6cb4da7ccb9a5ddee6e",
+    "url": "/kruki/static/js/main.a6cb4da7.chunk.js"
+  },
+  {
+    "revision": "3da870a3c1c4b41662f2",
+    "url": "/kruki/static/js/1.3da870a3.chunk.js"
+  },
+  {
+    "revision": "a6cb4da7ccb9a5ddee6e",
+    "url": "/kruki/static/css/main.46d0bfa4.chunk.css"
+  },
+  {
+    "revision": "b679b0d9fd1279e3978a582aaab20bbe",
+    "url": "/kruki/index.html"
+  }
+];
